c7b90a8e33bc1210e972672598df16b00b17a7e9
sathishinlinux <sathishinlinux@gmail.com>
2012-03-13 15:23:51 +0530

    Design

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,6 +7,7 @@ class HomeController < ApplicationController
 
   def index
    
+  
    @oauth = Koala::Facebook::OAuth.new(395715373788921, "8e63cfe50d07988ade86bc603da4b068","http://localhost:3000/")
    @oauth_code =   @oauth.get_app_access_token #(:permissions => "stream_publish") # @oauth.url_for_oauth_code
    @graph = Koala::Facebook::API.new(@oauth_code)
@@ -26,7 +27,8 @@ class HomeController < ApplicationController
   end
  	
   def twitter
-	
+	@client =Twitter.user("ziqqumusic")
+  @user_timeline = Twitter.user_timeline("ziqqumusic")
   end
   def test
         @client =Twitter.user("ziqqumusic")
